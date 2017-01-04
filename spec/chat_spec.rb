5d8f9ce2db41f9415b73ecfdd44ee423268c0975
jetpackpony <jetpackpony@gmail.com>
2017-01-04 22:19:16 +0300

    Add start and respond specs for chat

--- spec/chat_spec.rb
+++ spec/chat_spec.rb
@@ -2,9 +2,21 @@ require './lib/chat'
 include TravelBot
 
 RSpec.describe TravelBot::Chat do
+  describe "#start" do
+    it "calls the block with the first question from scenario" do
+      scenario = double()
+      allow(scenario).to receive(:current) { { question: "first question" } }
+      chat = Chat.new(scenario) do |msg|
+        expect(msg).to eq JSON.generate({ question: "first question" })
+      end
+
+      chat.start
+    end
+  end
   describe "#respond" do
     it "calls the block passed on object creation with message" do
-      chat = Chat.new do |msg|
+      scenario = double()
+      chat = Chat.new(scenario) do |msg|
         expect(msg).to eq "test message"
       end
       # This is a private method, so
