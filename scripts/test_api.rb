0e74b6a210d38489d3976aabbae081c37187088d
jetpackpony <jetpackpony@gmail.com>
2017-01-06 23:02:47 +0300

    Add flight live pricing api to api tests script

--- scripts/test_api.rb
+++ scripts/test_api.rb
@@ -1,8 +1,10 @@
-require './lib/skyscanner_api'
+require './lib/skyscanner_api/skyscanner_api'
 require 'byebug'
+require 'pry'
 require 'dotenv'
 Dotenv.load
 
+=begin
 req = SkyScannerAPI::LocationAutocompleteRequest.new
 req.apiKey = ENV["SKYSCANNERAPI_KEY"]
 req.market = "US"
@@ -14,3 +16,20 @@ req.send_request.then do |res|
   puts res.body
 end
 
+=end
+
+req = SkyScannerAPI::FlightsLivePricingRequest.new
+req.apiKey = ENV["SKYSCANNERAPI_KEY"]
+req.country = "US"
+req.currency = "USD"
+req.locale = "en-EN"
+req.origin = "MOSC-sky"
+req.destination = "BCN-sky"
+req.departureDate = Date.parse("july 4th").strftime("%Y-%m-%d")
+req.adultsNumber = 1
+req.locationSchema = "Sky"
+
+req.send_request.then do |res|
+  r = JSON.parse(res.body)
+  binding.pry
+end
