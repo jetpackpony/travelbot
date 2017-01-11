281a2c901ac8d22f893366d14a7b626afbe42b1b
jetpackpony <jetpackpony@gmail.com>
2017-01-11 22:42:15 +0300

    Move ChatForm to a separate file; Make chat work

--- travelbot-frontend/src/Chat.js
+++ travelbot-frontend/src/Chat.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
-import MessageList from './MessageList'
-
-class ChatForm extends Component {
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onSubmit(event) {
-    debugger;
-  }
-  render() {
-    return (
-      <div className="Form">
-        <form onSubmit={this.onSubmit}>
-          <input type="text" />
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    );
-  }
-}
+import MessageList from './MessageList';
+import ChatForm from './ChatForm';
 
 class Chat extends Component {
   constructor(props) {
     super(props);
     this.state = { messages: ["hello there", "hi"] };
-
     this.handleMessage = this.handleMessage.bind(this);
   }
 
@@ -38,7 +17,7 @@ class Chat extends Component {
     return (
       <div className="Chat">
         <MessageList messages={this.state.messages} />
-        <ChatForm onSubmit={this.handleMessage} />
+        <ChatForm handleMessage={this.handleMessage} />
       </div>
     );
   }
