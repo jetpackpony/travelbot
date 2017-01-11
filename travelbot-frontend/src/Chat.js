c0d4b79a9c8c2d015454469f0700c79e4b030a90
jetpackpony <jetpackpony@gmail.com>
2017-01-11 23:21:48 +0300

    Connect chat to websocket

--- travelbot-frontend/src/Chat.js
+++ travelbot-frontend/src/Chat.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import MessageList from './MessageList';
 import ChatForm from './ChatForm';
 
+const WS_ADDRESS = "ws://localhost:5000/";
+
 class Chat extends Component {
   constructor(props) {
     super(props);
-    this.state = { messages: ["hello there", "hi"] };
+    this.state = { messages: [] };
     this.handleMessage = this.handleMessage.bind(this);
+  }
+
+  componentDidMount() {
+    this.socket  = new WebSocket(WS_ADDRESS);
+    this.socket.onmessage = ({ data }) => {
+      this.setState({ messages: [data, ...this.state.messages] });
+    };
   }
 
   handleMessage(msg) {
     this.setState({ messages: [msg, ...this.state.messages] });
+    this.socket.send(msg);
   }
 
   render() {
