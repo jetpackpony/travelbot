ede3df6a5a6f1e0bd63dc893e9ba1f449af1b70a
jetpackpony <jetpackpony@gmail.com>
2017-01-12 21:06:58 +0300

    Move travelbot-web to a separate repository

--- travelbot-frontend/src/Message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function(props) {
-  return (
-    <div className={"message" + (props.ownMessage ? " message-mine" : "")}>
-      {props.message.label}
-    </div>
-  );
-}
