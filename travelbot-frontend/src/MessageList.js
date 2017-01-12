6180f8f6150f71e7667946f1132737f28cd0ceec
jetpackpony <jetpackpony@gmail.com>
2017-01-12 18:46:20 +0300

    Distinguish between user's own messages and other messages

--- travelbot-frontend/src/MessageList.js
+++ travelbot-frontend/src/MessageList.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 export default function (props) {
   const messageList = props.messages.map((msg, i) => {
-    return <li key={i}>{msg}</li>;
+    let string = msg.label;
+    if (msg.user && props.thisUser.id === msg.user.id) {
+      return <div key={i} className="message message-mine">{string}</div>;
+    } else {
+      return <div key={i} className="message">{string}</div>;
+    }
   });
 
   return (
     <div className="Message-list">
-      <ul>{messageList}</ul>
+      {messageList}
     </div>
   );
 }
