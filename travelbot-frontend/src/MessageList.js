626042606cadb5cfbf6dca4f907be947921fb8ae
jetpackpony <jetpackpony@gmail.com>
2017-01-12 18:56:57 +0300

    Extract a message component

--- travelbot-frontend/src/MessageList.js
+++ travelbot-frontend/src/MessageList.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Message from './Message';
 
 export default function (props) {
   const messageList = props.messages.map((msg, i) => {
-    let string = msg.label;
-    if (msg.user && props.thisUser.id === msg.user.id) {
-      return <div key={i} className="message message-mine">{string}</div>;
-    } else {
-      return <div key={i} className="message">{string}</div>;
-    }
+    return (
+      <Message
+        key={i}
+        ownMessage={msg.user && props.thisUser.id === msg.user.id}
+        message={msg}
+      />
+    );
   });
 
   return (
