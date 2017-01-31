6ccb6080780dd3e68d15bbc1bc53b3beef9db420
jetpackpony <jetpackpony@gmail.com>
2017-01-31 14:21:45 +0300

    Add logger to testing script

--- scripts/testing.rb
+++ scripts/testing.rb
@@ -1,9 +1,12 @@
 require './lib/chat'
 require './lib/scenario'
 require 'byebug'
+require 'logger'
+require 'pry'
 
+logger = Logger.new(STDOUT)
 scenario = TravelBot::Scenario.new
-chat = TravelBot::Chat.new(scenario) do |msg|
+chat = TravelBot::Chat.new(scenario, logger) do |msg|
   m = JSON.parse(msg)
   puts m["label"]
   if m["options"]
